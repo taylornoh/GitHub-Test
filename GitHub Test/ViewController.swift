5b79fd68cd4928c932cc6662a484e743af66264b
Taylor Noh <taylornoh99@gmail.com>
2020-09-24 14:55:29 -0700

    Initial Commit

---
+++ GitHub Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub Test
+//
+//  Created by Taylor Noh on 9/24/20.
+//  Copyright © 2020 Taylor Noh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitHub TestTests/GitHub_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitHub_TestTests.swift
+//  GitHub TestTests
+//
+//  Created by Taylor Noh on 9/24/20.
+//  Copyright © 2020 Taylor Noh. All rights reserved.
+//
+
+import XCTest
+@testable import GitHub_Test
+
+class GitHub_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
